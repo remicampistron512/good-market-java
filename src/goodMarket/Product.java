272544c64e6fe8e0960464f9cdd1679bb6c38004
Rémi Campistron <remi.campistron@gmail.com>
2025-11-13 15:48:49 +0100

    Création des attributs de la classe Product. Fixes #1

---
+++ src/goodMarket/Product.java
@@ -0,0 +1,16 @@
+package goodMarket;
+
+import java.time.LocalDate;
+
+public abstract class Product {
+    protected String name;
+    protected double unitPrice;
+    protected String unit;
+    protected double stockQuantity;
+    protected LocalDate pickingDate;
+    protected  int shelfLifeDays;
+
+    protected abstract LocalDate calculateExpirationDate();
+
+
+}
