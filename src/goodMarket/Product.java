a413958c2a12287eaa90715a981365259394d974
Rémi Campistron <remi.campistron@gmail.com>
2025-11-14 13:51:35 +0100

    Génére les getters/setters de la classe Product.fixes #26

--- src/goodMarket/Product.java
+++ src/goodMarket/Product.java
@@ -1,4 +1,4 @@
-package goodMarket;
+package goodmarket;
 
 import java.time.LocalDate;
 
@@ -10,4 +10,51 @@ public abstract class Product {
     protected LocalDate pickingDate;
     protected  int shelfLifeDays;
 
+    public String getName() {
+        return name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public double getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public LocalDate getPickingDate() {
+        return pickingDate;
+    }
+
+    public int getShelfLifeDays() {
+        return shelfLifeDays;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public void setStockQuantity(double stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public void setPickingDate(LocalDate pickingDate) {
+        this.pickingDate = pickingDate;
+    }
+
+    public void setShelfLifeDays(int shelfLifeDays) {
+        this.shelfLifeDays = shelfLifeDays;
+    }
 }
