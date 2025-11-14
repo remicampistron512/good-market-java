d3846c6a5b568bf8be073f17f03e770ba5d0d4b7
Rémi Campistron <remi.campistron@gmail.com>
2025-11-14 14:05:38 +0100

    Créer le constructeur de la classe Fruit.fixes #32

--- src/goodMarket/Fruit.java
+++ src/goodMarket/Fruit.java
@@ -3,6 +3,10 @@ package goodmarket;
 import java.time.LocalDate;
 
 public class Fruit extends Product implements IConsumable{
+    protected Fruit(String name, double unitPrice, String unit, double stockQuantity, LocalDate pickingDate, int shelfLifeDays) {
+        super(name, unitPrice, unit, stockQuantity, pickingDate, shelfLifeDays);
+    }
+
     @Override
     public boolean isExpired(LocalDate dateVerification) {
         return false;
