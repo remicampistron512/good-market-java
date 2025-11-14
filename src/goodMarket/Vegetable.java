3f247e5f8faea12cd50c9f109163f82f4f67588e
Rémi Campistron <remi.campistron@gmail.com>
2025-11-14 14:06:25 +0100

    Créer le constructeur de la classe Vegetable.fixes #33

--- src/goodMarket/Vegetable.java
+++ src/goodMarket/Vegetable.java
@@ -3,6 +3,10 @@ package goodmarket;
 import java.time.LocalDate;
 
 public class Vegetable extends Product implements IConsumable{
+    protected Vegetable(String name, double unitPrice, String unit, double stockQuantity, LocalDate pickingDate, int shelfLifeDays) {
+        super(name, unitPrice, unit, stockQuantity, pickingDate, shelfLifeDays);
+    }
+
     @Override
     public boolean isRipe() {
         return false;
